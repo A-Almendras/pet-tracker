638ed91c35e27cde85c15ef69b18e0cc3fa72645
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-13 15:15:18 -0400

    wip: trying to get side bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { useContext } from 'react'
+import AuthUserContext from '../context/AuthUserContext'
 
 const Nav = () => {
-  return <div>Welcome to the SideNavBar</div>
+  let { user } = useContext(AuthUserContext)
+  const items = [
+    {
+      title: 'Profile',
+      path: `/user/${user.username}`
+    },
+    {
+      title: 'Pets',
+      path: '/pets'
+    },
+    {
+      title: 'Logout',
+      path: '/'
+    }
+  ]
+  return (
+    <div className="navbar">
+      Welcome to the SideNavBar
+      {items.map((item, index) => (
+        <NavLink
+          to={item.path}
+          key={index}
+          className={({ isActive }) => (isActive ? 'active' : undefined)}
+        ></NavLink>
+      ))}
+    </div>
+  )
 }
 
 export default Nav
