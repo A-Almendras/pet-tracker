96883fc87e833cf322c0bbae1dc60557522d3124
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-13 15:12:16 -0400

    wip: setup petdash, petprofile, pets pages

---
+++ src/pages/PetProfile.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import axios from 'axios'
+
+const PetProfile = () => {
+  return <div>PetProfile</div>
+}
+
+export default PetProfile
--- src/pages/PetsPage.js
+++ src/pages/PetsPage.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 const PetsPage = () => {
+  // useEffect(() => {
+  //   const res = axios.get('http://localhost:8000/api/pets')
+  //   console.log('The app is responding fine', res)
+  // }, [])
+
   return <div>PetsPage</div>
 }
 
