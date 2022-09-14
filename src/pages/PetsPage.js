902d2dc946f276c2c2e0481ff4c2055e2e3e5576
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-14 09:13:08 -0400

    wip: implementing functionality to render user info and user pets...needs work, not working properly

--- src/pages/PetsPage.js
+++ src/pages/PetsPage.js
@@ -1,14 +1,42 @@
 import React from 'react'
-import axios from 'axios'
-import { useEffect } from 'react'
+import { useContext, useEffect } from 'react'
+import AuthUserContext from '../context/AuthUserContext'
+import { GetUserInfo, GetPets } from '../services/UserReq'
+import { useState } from 'react'
 
 const PetsPage = () => {
-  // useEffect(() => {
-  //   const res = axios.get('http://localhost:8000/api/pets')
-  //   console.log('The app is responding fine', res)
-  // }, [])
+  let { user, setUser, pets, renderUserPets } = useContext(AuthUserContext)
 
-  return <div>PetsPage</div>
+  const handleUserInfo = async (userId) => {
+    const data = await GetUserInfo(userId)
+    setUser(data)
+  }
+
+  const petDash = () => {}
+
+  useEffect(() => {
+    handleUserInfo(user.user_id)
+  }, [])
+
+  useEffect(() => {
+    renderUserPets(user.pets)
+  }, [pets])
+
+  console.log(user)
+  console.log({ pets })
+
+  return (
+    <div>
+      <h2>Choose a pet:</h2>
+      {user.pets}
+      {/* {pets.name} */}
+      <a href={user.pets}>Pets</a>
+      {pets?.map((pet, index) => (
+        <button key={index}>{pet.name}</button>
+      ))}
+      <button onClick={petDash}>+</button>
+    </div>
+  )
 }
 
 export default PetsPage
