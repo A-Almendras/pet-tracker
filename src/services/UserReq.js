0e7d8c2c085bc12c7861c1a8a63455d6a236bfe1
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-14 09:11:00 -0400

    wip: backend request to get user info and their pets data

--- src/services/UserReq.js
+++ src/services/UserReq.js
@@ -1,12 +1,22 @@
 import Client from './api'
-import jwt_decode from 'jwt-decode'
 
 //
 export const GetUserInfo = async (userId) => {
   try {
     const res = await Client.get(`users/${userId}`)
-    console.log(res)
-    return res
+    console.log(res.data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+//
+export const GetPets = async (userPets) => {
+  try {
+    const res = await Client.get(userPets)
+    console.log(res.data)
+    return res.data
   } catch (error) {
     throw error
   }
