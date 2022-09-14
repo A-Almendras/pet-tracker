dfff215be636a859afdb02deac42265c963afe3a
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-14 09:11:51 -0400

    wip: adding ids to routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import { useState } from 'react'
+import './styles/App.css'
 import { Route, Routes } from 'react-router-dom'
 import { useContext } from 'react'
 import AuthUserContext from './context/AuthUserContext'
+import Nav from './components/Nav'
 import LoginForm from './components/LoginForm'
 import RegisterForm from './components/RegisterForm'
 import LandingPage from './pages/LandingPage'
 import PetsPage from './pages/PetsPage'
-import Nav from './components/Nav'
+import UserPage from './pages/UserPage'
+import PetDash from './pages/PetDash'
 // import PrivateRoute from './services/PrivateRoute'
 
 function App() {
@@ -23,7 +26,9 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/user/:username" element={<UserPage />} />
         <Route path="/pets" element={<PetsPage />} />
+        <Route path="/pets/:petId" element={<PetDash />} />
       </Routes>
     </div>
   )
