12556b524d33ce21da05a0c1640dff5cb6c5d41b
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-15 22:07:05 -0400

    wip: create pet function

--- src/services/UserReq.js
+++ src/services/UserReq.js
@@ -21,3 +21,53 @@ export const GetPets = async () => {
     throw error
   }
 }
+//
+export const CreatePet = async (petInfo) => {
+  try {
+    const formData = new FormData()
+    formData.append('name', petInfo.name)
+    formData.append('animal_group', petInfo.animal_group)
+    formData.append('animal_kind', petInfo.animal_kind)
+    formData.append('dob', petInfo.dob)
+    formData.append('gotcha_date', petInfo.gotcha_date)
+    formData.append('age', petInfo.age)
+    formData.append('user_id', petInfo.user_id)
+    const res = await Client.post('pets/', formData)
+    console.log(res)
+  } catch (error) {
+    throw error
+  }
+}
+
+//
+// export const GetPetInfo = async (petId) => {
+//   try {
+//     const res = await Client.get(`pets/${petId}`)
+//     console.log(res.data)
+//     return res.data
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
+//
+// export const UpdatePetInfo = async (petId) => {
+//   try {
+//     const res = await Client.put(`pets/${petId}`)
+//     console.log(res.data)
+//     return res.data
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
+// //
+// export const DeletePet = async (petId) => {
+//   try {
+//     const res = await Client.delete(`pets/${petId}`)
+//     console.log(res.data)
+//     return res.data
+//   } catch (error) {
+//     throw error
+//   }
+// }
