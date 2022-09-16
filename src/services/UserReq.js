c070a03ce17c5ef7065810ddd57d91ff03637146
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-09-16 17:22:10 -0400

    wip: it seems that formData was giving me issue with fields not being able to be null so changed it

--- src/services/UserReq.js
+++ src/services/UserReq.js
@@ -24,16 +24,25 @@ export const GetPets = async () => {
 //
 export const CreatePet = async (petInfo) => {
   try {
-    const formData = new FormData()
-    formData.append('name', petInfo.name)
-    formData.append('animal_group', petInfo.animal_group)
-    formData.append('animal_kind', petInfo.animal_kind)
-    formData.append('dob', petInfo.dob)
-    formData.append('gotcha_date', petInfo.gotcha_date)
-    formData.append('age', petInfo.age)
-    formData.append('user_id', petInfo.user_id)
-    const res = await Client.post('pets/', formData)
+    // const formData = new FormData()
+    // formData.append('name', petInfo.name)
+    // formData.append('animal_group', petInfo.animal_group)
+    // formData.append('animal_kind', petInfo.animal_kind)
+    // formData.append('dob', petInfo.dob)
+    // formData.append('gotcha_date', petInfo.gotcha_date)
+    // formData.append('age', petInfo.age)
+    // formData.append('user_id', petInfo.user_id)
+    const res = await Client.post('pets/', {
+      name: petInfo.name,
+      animal_group: petInfo.animal_group,
+      animal_kind: petInfo.animal_kind,
+      dob: petInfo.dob,
+      gotcha_date: petInfo.gotcha_date,
+      age: petInfo.age,
+      user_id: petInfo.user_id
+    })
     console.log(res)
+    return res.data
   } catch (error) {
     throw error
   }
@@ -50,21 +59,20 @@ export const DeletePetProfile = async (petId) => {
   }
 }
 
+export const UpdatePetInfo = async (petId, petInfo) => {
+  try {
+    const res = await Client.put(`pets/${petId}`, petInfo)
+    console.log(res)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
 //
 // export const GetPetInfo = async (petId) => {
 //   try {
 //     const res = await Client.get(`pets/${petId}`)
-//     console.log(res.data)
-//     return res.data
-//   } catch (error) {
-//     throw error
-//   }
-// }
-
-//
-// export const UpdatePetInfo = async (petId, petInfo) => {
-//   try {
-//     const res = await Client.put(`pets/${petId}`, petInfo)
 //     console.log(res.data)
 //     return res.data
 //   } catch (error) {
